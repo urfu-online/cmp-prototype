16600a197aa60abc0b3d11a884bb2169ae9271b7
lea_alex <lea_alex@mail.ru>
2020-12-14 12:53:02 +0500

    правки 6

--- src/pages/AgreementPage.js
+++ src/pages/AgreementPage.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import contract from "../static/offer.pdf";
 
 const AgreementPage = () => {
   return (
-        <>
-            <div className="container">
-                Agreement page
-            </div>
-
-        </>
+      <>
+          <h2>Соглашение</h2>
+          <a href={contract} className="btn btn-primary" download>Скачать</a>
+      </>
   )
 }
 
